11261d986a5e6fcaf38fddc77ee78fe58d250ee8
John Martin <48291409+jomartin-999@users.noreply.github.com>
2021-12-01 17:09:47 -0500

    Simple Hello World script

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 from rediscluster import RedisCluster
 
-def simple_redis_demo():
-    startup_nodes = [{"host": "127.0.0.1", "port": "6379"}]
+def hello_world():
+
+    redisCluster = RedisCluster(
+        host="127.0.0.1",
+        port=6379,
+        decode_responses=True,
+        skip_full_coverage_check=True,
+    )
+    redisCluster.set("MyFirstValue", "Hello World")
+    print(redisCluster.get("MyFirstValue"))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    simple_redis_demo()
+    hello_world()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
